28e5eaca30d12f9e4ab5f44a342e38d09b721e45
Andrew Bourinski <andrew.bourinski@student.allenisd.org>
2022-05-09 00:14:58 -0500

    updated comments

--- Sources/ScenesShell/InteractionLayer.swift
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -7,6 +7,7 @@ class InteractionLayer : Layer {
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Interaction")
 
+        
         // We insert our RenderableEntities in the constructor
         insert(entity: ball, at: .front)
     }
